1e988bf1f04efd8837bd79e6c5b545c2c0706564
Emma Weberyd <emma.weberyd@gmail.com>
2018-03-20 10:35:50 +0100

    add navigation with tabs and swipe

--- App.js
+++ App.js
@@ -2,24 +2,17 @@ import React, { Component } from 'react';
 import QRScanner from './Screens/QRScanner';
 import HomeScreen from './Screens/HomeScreen';
 import Profile from './Screens/Profile';
-import Chat from './Screens/Chat';
-import { StackNavigator } from 'react-navigation';
+import { TabNavigator } from 'react-navigation';
 
-export default class App extends Component {
-  render() {
-    return <NavigationApp/>;
-  }
-}
-
-const NavigationApp = StackNavigator(
+export default TabNavigator(
   {
+    Profile: { screen: Profile },
     Home: { screen: HomeScreen },
     Scanner: { screen: QRScanner },
-    Profile: { screen: Profile },
-    Chat: { screen: Chat },
   },
   {
     initialRouteName: 'Home',
-    headerMode: 'none',
+    swipeEnabled: true,
+    tabBarPosition: 'bottom',
   }
 );
