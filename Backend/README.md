96e52cf7536e7b594976dcdf6d57f69f116b0f8f
David Eriksson <david.eriksson@lexiconitkonsult.se>
2018-03-23 10:59:35 +0100

    the beckend code and a README file that explains how it works.

---
+++ Backend/README.md
@@ -0,0 +1,61 @@
+# How to use the RESTful webservice
+
+The backend root can be found at http://83.227.100.223:8080. This doesn't work on iOS since it's not a https connection but this will be fixed eventually.
+
+## http://83.227.100.223:8080/create/
+this http-request will create a chatroom and return the roomID.
+
+Input:
+none
+
+Output:
+LAST_INSERT_ID():	X
+
+## http://83.227.100.223:8080/connect/:roomID
+this http-request will connect to a chatroom and alter the connected state from 0 to 1.
+
+Input:
+:roomID - the ID of the room you wish to connect to 
+
+Output:
+"Connected"
+
+## http://83.227.100.223:8080/messages/:roomID/
+this http-request will connect to a chatroom and return all the messages that has ever been written in that room.
+
+Input:
+:roomID - the ID of the room you wish to connect to 
+
+Output:
+0:{
+	roomID : X
+	message : "text"
+	sentby: 1 or 2 (depending on who sent the message)
+	send_time: "YYYY-MM-DDTHH:MM:SS.000Z"
+}
+
+## http://83.227.100.223:8080/messages/:roomID/:time
+this http-request will connect to a chatroom and return all the messages that has a send_time after the time given.
+
+Input:
+:roomID - the ID of the room you wish to connect to
+:time - the time that will be used to filter the output
+
+Output:
+0:{
+	roomID : X
+	message : "text"
+	sentby: 1 or 2 (depending on who sent the message)
+	send_time: "YYYY-MM-DDTHH:MM:SS.000Z"
+}
+
+## http://83.227.100.223:8080/submit/:roomID/:message/:sentby
+this http-request will connect to a chatroom and submit a message to the database.
+
+Input:
+:roomID - the ID of the room you wish to connect to.
+:message - the message that will be submited.
+:sentby - either 1 or 2 depending on who sent the message.
+
+Output:
+None
---
+++ Backend/server.js
@@ -0,0 +1,109 @@
+const express = require("express");
+
+var mysql = require('mysql');
+
+// Initialize http server
+const app = express();
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "134GF1570a",
+  database: "TheMainFrame"
+});
+
+
+
+// Handle / route
+app.get('/', function (req, res){
+  res.send('Welcome to the mainframe!')
+})
+
+app.get('/messages/:roomID/', function (req, res){
+	
+  		var sql = "SELECT * FROM chat_messages WHERE roomID = " + mysql.escape(req.params.roomID);
+  		con.query(sql , function (err, result, fields) {
+    		if (err) {
+				res.send("no result!");//throw err;
+    		}
+    		else{
+    			res.send(result);
+    		}
+  		});
+})
+app.get('/messages/:roomID/:time/', function (req, res){
+		
+		var time = req.params.time;
+		//time = time.replace('T', ' ');
+		//time = time.replace('.000Z', '');
+		//time = time + "+2:00";
+		var sql = "SELECT * FROM chat_messages WHERE roomID = " + mysql.escape(req.params.roomID) + " AND (TIMESTAMPDIFF(second,send_time," +mysql.escape(time)+ ") < -3600)";
+		//var sql = "SELECT TIMESTAMPDIFF(second,sent_time," + mysql.escape(time) + ") FROM chat_messages";
+  		con.query(sql , function (err, result, fields) {
+    		if (err) {
+				//res.send("no result!");
+				throw err;
+    		}
+    		else{
+    			res.send(result);
+    		}
+  		});
+  		//res.send(sql);
+})
+app.get('/submit/:roomID/:message/:sentby', function (req, res){
+	  	
+		var sql = "SELECT connected FROM chatts WHERE roomID = "+ mysql.escape(req.params.roomID);
+		con.query(sql,function(err, result){
+			if(err) throw err;
+			if(result[0].connected == 0){
+				res.send("No other user connected to this chat room!");
+			}
+			else{
+				var sql = "INSERT INTO chat_messages (roomID, message,sentby) VALUES ("+ mysql.escape(req.params.roomID) + "," + mysql.escape(req.params.message) + "," + mysql.escape(req.params.sentby) + ")";
+			  	con.query(sql, function (err, result)
+			  	{
+			    	if (err) throw err;
+			    	console.log("1 record inserted");
+			  	});
+			  	res.send("");
+			}
+		})
+	  	
+})
+
+app.get('/create/', function(req,res){
+	var sql ="INSERT INTO chatts(connected) VALUES (0)";
+	con.query(sql, function(err,result){
+		if(err) throw err;
+		console.log("Chatt created!");
+	})
+	var sql2 = "SELECT LAST_INSERT_ID()";
+		con.query(sql2, function(err,result){
+		if(err) throw err;
+		res.send(result);
+	})
+
+})
+app.get('/connect/:roomID', function(req,res){
+	var sql = "UPDATE chatts SET connected=1 WHERE roomID = " + mysql.escape(req.params.roomID);
+	con.query(sql, function(err, result){
+		if(err) throw err;
+		console.log("Connected to chatt");
+	})
+	res.send("Connected");
+})
+app.get('/listchatts/',function(req,res){
+	var sql = "SELECT * FROM chatts";
+	con.query(sql, function(err,result){
+		if(err) throw err;
+		res.send(result);
+	})
+})
+
+
+// Launch the server on port 3000
+const server = app.listen(8080, () => {
+  const { address, port } = server.address();
+  console.log(`Listening at http://${address}:${port}`);
+});
