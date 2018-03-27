2a807758ea6ca220941c0b274259c713ec57f42b
Emma Weberyd <emma.weberyd@gmail.com>
2018-03-27 14:50:50 +0200

    add navigator

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import QRScanner from './Screens/QRScanner';
-import HomeScreen from './Screens/HomeScreen';
 import Profile from './Screens/Profile';
 import Chat from './Screens/Chat';
-import { TabNavigator } from 'react-navigation';
+import HomeScreen from './Screens/HomeScreen';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
-export default TabNavigator(
+const TabNav = TabNavigator(
   {
     Profile: { screen: Profile },
     Home: { screen: HomeScreen },
@@ -16,5 +16,25 @@ export default TabNavigator(
     initialRouteName: 'Home',
     swipeEnabled: true,
     tabBarPosition: 'bottom',
+  }
+);
+
+export default StackNavigator(
+  {
+    Home: {
+      screen: TabNav,
+      navigationOptions: ({ navigation }) => ({
+        header: null,
+      }),
+    },
+    Chat: {
+      screen: Chat,
+      navigationOptions: ({ navigation }) => ({
+        title: 'Chat name',
+      }),
+    },
+  },
+  {
+    initialRouteName: 'Home',
   }
 );
