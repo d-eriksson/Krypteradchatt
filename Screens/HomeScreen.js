9c4a9f88b4a32aaa443eb5baa63cd0f5534638d8
JohnnyMelin <johnny.melin95@gmail.com>
2018-03-28 09:01:20 +0200

    ändrade generateQR.js så att den nu tar in tre element och sätter ihop dom till en sträng och testar fetch i homescreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -9,17 +9,43 @@ import {
 
 export default class HomeScreen extends Component {
 
+	constructor (props){
+		super(props);
+		this.state = {
+			things:{}
+		};
+	}
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.wrapper}>
-                <View>
-                       <Image />
-                       <View>
-                            <Text>Namn</Text>
-                            <Text>Senaste Chattmeddelandet</Text>
-                       </View>
-                </View>
+			<View>
+			<Image />
+				<View>
+					<Text>Namn</Text>
+					<Text>Senaste Chattmeddelandet</Text>
+				</View>
+				<Button 
+					title='Hej'
+					value='Okej'
+					sign='___'
+					key='Key'
+					name='Cool Chatt'
+					onPress={() => {
+					fetch('http://83.227.100.223:8080/create')
+					.then((response) => response.json)
+					.then((responseJson) => {
+					this.setState({
+						things: responseJson
+					})
+					alert(this.state.things)
+					})
+					}}
+						
+						
+				/>
+
+			</View>
           </View>
       </View>
     );
