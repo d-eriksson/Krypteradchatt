fb7cd791f889a9407a2b0e9394918e4af61d4bc4
Emma Weberyd <emma.weberyd@gmail.com>
2018-04-12 10:26:27 +0200

    fixing things

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -25,56 +25,24 @@ export default class HomeScreen extends Component {
 	}
   render() {
     return (
-<<<<<<< HEAD
       <View>
-          <View>
-    					<ChatList/>
-    			</View>
-          <View>
-      				<Button
-      					title='Hej'
-      					onPress={() => {
-
-      					fetch('http://83.227.100.223:8080/create')
-      					.then((res) => res.json())
-      					.then((data) => {
-      					this.setState({things: data})
-      					alert(this.state.things)	})
-      					.catch((err) => alert(err))
-      					}}
-      				/>
-=======
-      <View style={styles.container}>
-        <View style={styles.wrapper}>
-			<View>
-			<Image />
-				<View>
-					<Text>Namn</Text>
-					<Text>Senaste Chattmeddelandet</Text>
-				</View>
-				<Button
-					title='Create Chatt'
-					onPress={() => {
-
-					fetch('http://83.227.100.223:8080/create')
-					.then((res) => res.json())
-					.then((data) => {
-					this.setState({
-            roomID: data,
-            hasch: SHA.sha256("Hasch"),
-            fullString: data.toString() + this.state.sign + this.state.chatname + this.state.sign + this.state.hasch
-          });
-					console.log(this.state.fullString);
-          <GenerateQR value={this.state.fullString}/>})
-					.catch((err) => alert(err))
-					}}
-				/>
-
-
-
-			</View>
->>>>>>> Task016
-          </View>
+        <ChatList/>
+        <Button
+          title='Create Chatt'
+          onPress={() => {
+            fetch('http://83.227.100.223:8080/create')
+            .then((res) => res.json())
+            .then((data) => {
+              this.setState({
+                roomID: data,
+                hasch: SHA.sha256("Hasch"),
+                fullString: data.toString() + this.state.sign + this.state.chatname + this.state.sign + this.state.hasch
+              });
+              console.log(this.state.fullString);
+              <GenerateQR value={this.state.fullString}/>})
+              .catch((err) => alert(err))
+            }}
+        />
       </View>
     );
   };
