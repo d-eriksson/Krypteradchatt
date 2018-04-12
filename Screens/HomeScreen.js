2cdc400a36270677e28f1b3af940acdb7d5d67cd
Messiiha <35451314+Messiiha@users.noreply.github.com>
2018-04-12 10:05:03 +0200

    Addering av strängar i QRKod vid skapandet av chatt

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -7,13 +7,21 @@ import {
   Image,
   } from 'react-native';
 
+import GenerateQR from '../Components/generateQR';
+import * as SHA from 'js-sha256';
 export default class HomeScreen extends Component {
 
 	constructor (props){
 		super(props);
 		this.state = {
-			things:{}
+			roomID: {},
+      hasch: '',
+      sign: '___',
+      chatname: 'Cool Chatt',
+      fullstring: ''
 		};
+
+
 	}
   render() {
     return (
@@ -25,22 +33,25 @@ export default class HomeScreen extends Component {
 					<Text>Namn</Text>
 					<Text>Senaste Chattmeddelandet</Text>
 				</View>
-				<Button 
-					title='Hej'
-					value='Okej'
-					sign='___'
-					key='Key'
-					name='Cool Chatt'
+				<Button
+					title='Create Chatt'
 					onPress={() => {
-				
+
 					fetch('http://83.227.100.223:8080/create')
 					.then((res) => res.json())
 					.then((data) => {
-					this.setState({things: data})
-					alert(this.state.things)	})
+					this.setState({
+            roomID: data,
+            hasch: SHA.sha256("Hasch"),
+            fullString: data.toString() + this.state.sign + this.state.chatname + this.state.sign + this.state.hasch
+          });
+					console.log(this.state.fullString);
+          <GenerateQR value={this.state.fullString}/>})
 					.catch((err) => alert(err))
 					}}
 				/>
+
+
 
 			</View>
           </View>
